d9333af51ec070a012e8987c9f4745969075561e
Шелков Дмитрий <bobyorrr@gmail.com>
2021-05-13 14:30:22 +0300

    start

---
+++ main.js
@@ -0,0 +1,7 @@
+import "./style.css";
+import "./style.scss";
+
+document.querySelector("#app").innerHTML = `
+  <h1>Ты хуй</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
