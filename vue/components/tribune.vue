9ea8a57f57bbeb742ccc5d9fc717695535bbb7cd
Шелков Дмитрий <bobyorrr@gmail.com>
2021-05-20 14:24:32 +0300

    растоновка + персоонажи

--- vue/components/tribune.vue
+++ vue/components/tribune.vue
@@ -1,174 +1,24 @@
 <template>
     <div class="tribune" ref="tribuneEl">
-        <div class="tribune__row">
-            <div class="tribune__plase" style="width: calc(75px * 2)"></div>
+        <div class="tribune__row" v-for="(row, key) in map" :key="key">
             <div
                 class="tribune__plase"
-                style="width: calc(75px * 5); background-position-y: -93px"
-            ></div>
-            <div
-                class="tribune__plase"
-                style="width: calc(75px * 5); background-position-y: -186px"
+                v-for="(plase, plaseKey) in row.plaseList"
+                :key="'plase_' + plaseKey"
+                :style="plase.style"
             ></div>
             <person-animait
-                :urlSprite="'url(' + imgUrl.sushi_8 + ')'"
-                style="left: 40px"
-                @setAnimat="setAnimat"
+                v-for="(person, personKey) in row.persons"
+                :key="'person__' + personKey"
+                :urlSprite="'url(' + person.img + ')'"
+                :mainСharacter="
+                    person.mainСharacter ? person.mainСharacter : false
+                "
+                :style="person.style"
             ></person-animait>
-            <person-animait
-                :urlSprite="'url(' + imgUrl.sushi_9 + ')'"
-                style="left: 240px"
-                @setAnimat="setAnimat"
-            ></person-animait>
-            <person-animait
-                :urlSprite="'url(' + imgUrl.sushi_10 + ')'"
-                style="left: 440px"
-                @setAnimat="setAnimat"
-            ></person-animait>
-            <person-animait
-                :urlSprite="'url(' + imgUrl.sushi_0 + ')'"
-                style="left: 640px"
-                @setAnimat="setAnimat"
-            ></person-animait>
-        </div>
-        <div class="tribune__row">
-            <div class="tribune__plase" style="width: calc(75px * 3)"></div>
-            <div
-                class="tribune__plase"
-                style="width: calc(75px * 5); background-position-y: -93px"
-            ></div>
-            <div
-                class="tribune__plase"
-                style="width: calc(75px * 5); background-position-y: -186px"
-            ></div>
-            <person-animait
-                :urlSprite="'url(' + imgUrl.sushi_4 + ')'"
-                style="left: 40px"
-                @setAnimat="setAnimat"
-            ></person-animait>
-            <person-animait
-                :urlSprite="'url(' + imgUrl.sushi_5 + ')'"
-                style="left: 240px"
-                @setAnimat="setAnimat"
-            ></person-animait>
-            <person-animait
-                :urlSprite="'url(' + imgUrl.sushi_6 + ')'"
-                style="left: 440px"
-                @setAnimat="setAnimat"
-            ></person-animait>
-            <person-animait
-                :urlSprite="'url(' + imgUrl.sushi_7 + ')'"
-                style="left: 640px"
-                @setAnimat="setAnimat"
-            ></person-animait>
-        </div>
-        <div class="tribune__row">
-            <div class="tribune__plase" style="width: calc(75px * 3)"></div>
-            <div
-                class="tribune__plase"
-                style="width: calc(75px * 5); background-position-y: -93px"
-            ></div>
-            <div
-                class="tribune__plase"
-                style="width: calc(75px * 5); background-position-y: -186px"
-            ></div>
-            <person-animait
-                :urlSprite="'url(' + imgUrl.sushi_0 + ')'"
-                style="left: 40px"
-                @setAnimat="setAnimat"
-            ></person-animait>
-            <person-animait
-                :urlSprite="'url(' + imgUrl.sushi_1 + ')'"
-                style="left: 240px"
-                @setAnimat="setAnimat"
-            ></person-animait>
-            <person-animait
-                :urlSprite="'url(' + imgUrl.char_1 + ')'"
-                :mainСharacter="true"
-                style="left: 440px"
-                @setAnimat="setAnimat"
-            ></person-animait>
-            <person-animait
-                :urlSprite="'url(' + imgUrl.sushi_3 + ')'"
-                style="left: 640px"
-                @setAnimat="setAnimat"
-            ></person-animait>
-        </div>
-        <div class="tribune__row">
-            <div class="tribune__plase" style="width: calc(75px * 4)"></div>
-            <div
-                class="tribune__plase"
-                style="width: calc(75px * 5); background-position-y: -93px"
-            ></div>
-            <div
-                class="tribune__plase"
-                style="width: calc(75px * 4); background-position-y: -186px"
-            ></div>
-            <person-animait
-                :urlSprite="'url(' + imgUrl.sushi_8 + ')'"
-                style="left: 40px"
-                @setAnimat="setAnimat"
-            ></person-animait>
-            <person-animait
-                :urlSprite="'url(' + imgUrl.sushi_9 + ')'"
-                style="left: 240px"
-                @setAnimat="setAnimat"
-            ></person-animait>
-            <person-animait
-                :urlSprite="'url(' + imgUrl.sushi_10 + ')'"
-                style="left: 440px"
-                @setAnimat="setAnimat"
-            ></person-animait>
-            <person-animait
-                :urlSprite="'url(' + imgUrl.sushi_0 + ')'"
-                style="left: 640px"
-                @setAnimat="setAnimat"
-            ></person-animait>
-        </div>
-        <div class="tribune__row">
-            <div class="tribune__plase" style="width: calc(75px * 4)"></div>
-            <div
-                class="tribune__plase"
-                style="width: calc(75px * 5); background-position-y: -93px"
-            ></div>
-            <div
-                class="tribune__plase"
-                style="width: calc(75px * 4); background-position-y: -186px"
-            ></div>
-            <person-animait
-                :urlSprite="'url(' + imgUrl.sushi_4 + ')'"
-                style="left: 40px"
-                @setAnimat="setAnimat"
-            ></person-animait>
-            <person-animait
-                :urlSprite="'url(' + imgUrl.sushi_5 + ')'"
-                style="left: 240px"
-                @setAnimat="setAnimat"
-            ></person-animait>
-            <person-animait
-                :urlSprite="'url(' + imgUrl.sushi_6 + ')'"
-                style="left: 440px"
-                @setAnimat="setAnimat"
-            ></person-animait>
-            <person-animait
-                :urlSprite="'url(' + imgUrl.sushi_7 + ')'"
-                style="left: 640px"
-                @setAnimat="setAnimat"
-            ></person-animait>
-        </div>
-        <div class="tribune__row">
-            <div class="tribune__plase" style="width: calc(75px * 5)"></div>
-            <div
-                class="tribune__plase"
-                style="width: calc(75px * 5); background-position-y: -93px"
-            ></div>
-            <div
-                class="tribune__plase"
-                style="width: calc(75px * 3); background-position-y: -186px"
-            ></div>
         </div>
         <div class="action-button panel-action-button" @click="setGreatPoint">
-            <button-g name="Прыгай" :disabled="jumpButtonClose"></button-g>
+            <button-g name="Прыгай!" :disabled="jumpButtonClose"></button-g>
         </div>
     </div>
 </template>
@@ -177,19 +27,13 @@ import personAnimait from "./personAnimait.vue";
 import buttonG from "../ui/button.vue";
 import { mapGetters, mapMutations } from "vuex";
 
-import sushi_0 from "../sprite/person/0_sushi.png";
-import sushi_1 from "../sprite/person/1_sushi.png";
-import sushi_2 from "../sprite/person/2_sushi.png";
-import sushi_3 from "../sprite/person/3_sushi.png";
-import sushi_4 from "../sprite/person/4_sushi.png";
-import sushi_5 from "../sprite/person/5_sushi.png";
-import sushi_6 from "../sprite/person/6_sushi.png";
-import sushi_7 from "../sprite/person/7_sushi.png";
-import sushi_8 from "../sprite/person/8_sushi.png";
-import sushi_9 from "../sprite/person/9_sushi.png";
-import sushi_10 from "../sprite/person/10_sushi.png";
-import bunny_4 from "../sprite/person/4_bunny.png";
-import char_1 from "../sprite/person/char_1.png";
+const modules = import.meta.globEager("../sprite/person/*.png");
+
+let listImg = {};
+for (const key in modules) {
+    const nameArr = key.split("/");
+    listImg[nameArr[nameArr.length - 1]] = modules[key];
+}
 
 export default {
     components: {
@@ -200,32 +44,231 @@ export default {
         return {
             animat: "happy",
             jump: 22,
-            imgUrl: {
-                sushi_0,
-                sushi_1,
-                sushi_2,
-                sushi_3,
-                sushi_4,
-                sushi_5,
-                sushi_6,
-                sushi_7,
-                sushi_8,
-                sushi_9,
-                sushi_10,
-                bunny_4,
-                char_1,
-            },
+            map: [
+                {
+                    plaseList: [
+                        {
+                            style:
+                                "width: calc(75px * 2); background-position-y: -186px",
+                        },
+                        {
+                            style: "width: calc(75px * 5)",
+                        },
+                        {
+                            style:
+                                "width: calc(75px * 5); background-position-y: -93px",
+                        },
+                    ],
+                    persons: [
+                        {
+                            img: listImg["0_sushi.png"].default,
+                            style: "left: 90px; transform: scale(0.8);",
+                        },
+                        {
+                            img: listImg["7_sushi.png"].default,
+                            style:
+                                "left: 310px; transform: scale(0.7); bottom: -40px",
+                        },
+                        {
+                            img: listImg["1_sushi.png"].default,
+                            style: "left: 470px; transform: scale(0.8);",
+                        },
+                        {
+                            img: listImg["2_sushi.png"].default,
+                            style: "left: 616px; transform: scale(0.66)",
+                        },
+                    ],
+                },
+                {
+                    plaseList: [
+                        {
+                            style:
+                                "width: calc(75px * 3); background-position-y: -186px",
+                        },
+                        {
+                            style: "width: calc(75px * 5)",
+                        },
+                        {
+                            style:
+                                "width: calc(75px * 5); background-position-y: -93px",
+                        },
+                    ],
+                    persons: [
+                        {
+                            img: listImg["0_player.png"].default,
+                            style:
+                                "left: 37px; transform: scale(0.95);bottom: -57px",
+                        },
+                        {
+                            img: listImg["tanuki.png"].default,
+                            style: "left: 237px; bottom: -40px",
+                        },
+                        {
+                            img: listImg["5_sushi.png"].default,
+                            style:
+                                "left: 418px; transform: scale(0.6);bottom: -45px",
+                        },
+                        {
+                            img: listImg["3_sushi.png"].default,
+                            style:
+                                "left: 532px; transform: scale(0.66);bottom:-50px",
+                        },
+                        {
+                            img: listImg["4_sushi.png"].default,
+                            style:
+                                "left: 724px; transform: scale(0.8);bottom: -35px",
+                        },
+                    ],
+                },
+                {
+                    plaseList: [
+                        {
+                            style:
+                                "width: calc(75px * 3); background-position-y: -186px",
+                        },
+                        {
+                            style: "width: calc(75px * 5)",
+                        },
+                        {
+                            style:
+                                "width: calc(75px * 5); background-position-y: -93px",
+                        },
+                    ],
+                    persons: [
+                        {
+                            img: listImg["4_bunny.png"].default,
+                            style:
+                                "left: 88px; transform: scale(1.1);bottom: -32px",
+                        },
+                        {
+                            img: listImg["char_1.png"].default,
+                            mainСharacter: true,
+                            style:
+                                "left: 306px; transform: scale(1.1);bottom: -35px",
+                        },
+                        {
+                            img: listImg["10_sushi.png"].default,
+                            style:
+                                "left: 457px; transform: scale(0.8);bottom: -42px",
+                        },
+                        {
+                            img: listImg["1_bunny.png"].default,
+                            style:
+                                "left: 582px; transform: scale(1);bottom: -15px",
+                        },
+                    ],
+                },
+                {
+                    plaseList: [
+                        {
+                            style:
+                                "width: calc(75px * 4); background-position-y: -186px",
+                        },
+                        {
+                            style: "width: calc(75px * 5)",
+                        },
+                        {
+                            style:
+                                "width: calc(75px * 5); background-position-y: -93px",
+                        },
+                    ],
+                    persons: [
+                        {
+                            img: listImg["2_bunny.png"].default,
+                            style:
+                                "left: 21px; transform: scale(1);bottom: -32px",
+                        },
+                        {
+                            img: listImg["8_sushi.png"].default,
+                            style:
+                                "left: 156px; transform: scale(0.66);bottom: -47px",
+                        },
+                        {
+                            img: listImg["5_player.png"].default,
+                            style:
+                                "left: 241px; transform: scale(1.1);bottom: -30px",
+                        },
+                        {
+                            img: listImg["6_sushi.png"].default,
+                            style:
+                                "left: 386px; transform: scale(0.8);bottom: -50px",
+                        },
+                        {
+                            img: listImg["9_sushi.png"].default,
+                            style:
+                                "left: 572px; transform: scale(0.8);bottom: -28px",
+                        },
+                    ],
+                },
+                {
+                    plaseList: [
+                        {
+                            style:
+                                "width: calc(75px * 4); background-position-y: -186px",
+                        },
+                        {
+                            style: "width: calc(75px * 5)",
+                        },
+                        {
+                            style:
+                                "width: calc(75px * 5); background-position-y: -93px",
+                        },
+                    ],
+                    persons: [],
+                },
+                {
+                    plaseList: [
+                        {
+                            style:
+                                "width: calc(75px * 5); background-position-y: -186px",
+                        },
+                        {
+                            style: "width: calc(75px * 5)",
+                        },
+                        {
+                            style:
+                                "width: calc(75px * 5); background-position-y: -93px",
+                        },
+                    ],
+                    persons: [
+                        {
+                            img: listImg["3_bunny.png"].default,
+                            style:
+                                "left: 48px; transform: scale(1);bottom: -6px",
+                        },
+                        {
+                            img: listImg["4_player.png"].default,
+                            style:
+                                "left: 164px; transform: scale(0.8);bottom: -23px",
+                        },
+                        {
+                            img: listImg["3_player.png"].default,
+                            style:
+                                "left: 335px; transform: scale(1);bottom: -9px",
+                        },
+                        {
+                            img: listImg["2_player.png"].default,
+                            style:
+                                "left: 470px; transform: scale(1);bottom: -9px",
+                        },
+                        {
+                            img: listImg["1_player.png"].default,
+                            style:
+                                "left: 598px; transform: scale(0.9);bottom: -2px",
+                        },
+                        {
+                            img: listImg["0_bunny.png"].default,
+                            style:
+                                "left: 722px; transform: scale(1);bottom: 10px",
+                        },
+                    ],
+                },
+            ],
         };
     },
-    // watch: {
-    //     jumpStore: function (val) {
-    //         this.setAnimat("jump");
-    //     },
-    // },
     computed: {
         tribuneRow() {},
         ...mapGetters({
-            //jumpStore: "jump",
             jumpButtonClose: "jumpButton",
         }),
     },
@@ -235,21 +278,17 @@ export default {
         },
         ...mapMutations({
             setGreatPoint: "setGreatPoint",
-            // setAnimat: "setAnimat",
-            // setJump: "setJump"
         }),
     },
     created() {
         console.log(this.test);
-    },
-    mounted() {
-        // console.log(this.$refs.tribuneEl);
     },
 };
 </script>
 <style lang="scss">
 .panel-action-button {
     z-index: 1;
+    margin-top: -20px;
 }
 .tribune {
     width: 100%;
